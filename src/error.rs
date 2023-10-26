dd1af3614967eb455a23a6520520b70e5cadcb77
Mike Dilger <mike@mikedilger.com>
2023-10-26 18:57:15 +1300

    Config, and loading at startup

--- src/error.rs
+++ src/error.rs
@@ -3,6 +3,10 @@ use thiserror::Error;
 /// Errors that can occur in the chorus crate
 #[derive(Error, Debug)]
 pub enum Error {
+    // Config
+    #[error("Config: {0}")]
+    Config(#[from] ron::error::SpannedError),
+
     // I/O Error
     #[error("I/O: {0}")]
     Io(#[from] std::io::Error),
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,29 @@
+
+pub mod config;
 pub mod error;
 
+use crate::config::Config;
 use crate::error::Error;
+use std::env;
+use std::fs::OpenOptions;
+use std::io::Read;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
+    // Get args (config path)
+    let mut args = env::args();
+    if args.len() <= 1 {
+        panic!("USAGE: chorus <config_path>");
+    }
+    let _ = args.next(); // ignore program name
+    let config_path = args.next().unwrap();
+
+    // Read config file
+    let mut file = OpenOptions::new().read(true).open(config_path)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    let config: Config = ron::from_str(&contents)?;
+
     println!("No main yet.");
 
     Ok(())
