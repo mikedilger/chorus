8e5917e6005a71562fe4ca696a78284c2812a7fd
Mike Dilger <mike@mikedilger.com>
2023-10-26 19:07:37 +1300

    depend on hyper

--- src/error.rs
+++ src/error.rs
@@ -7,6 +7,14 @@ pub enum Error {
     #[error("Config: {0}")]
     Config(#[from] ron::error::SpannedError),
 
+    // Http
+    #[error("HTTP: {0}")]
+    Http(#[from] hyper::http::Error),
+
+    // Hyper
+    #[error("Hyper: {0}")]
+    Hyper(#[from] hyper::Error),
+
     // I/O Error
     #[error("I/O: {0}")]
     Io(#[from] std::io::Error),
