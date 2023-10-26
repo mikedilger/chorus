0fb73f20f227f1ab275f82ab2e6c4a98831b4698
Mike Dilger <mike@mikedilger.com>
2023-10-26 20:05:10 +1300

    use_tls setting

--- src/config.rs
+++ src/config.rs
@@ -1,9 +1,10 @@
-use serde::{Deserialize, Serialize};
+use serde::{Serialize, Deserialize};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Config {
     pub ip_address: String,
     pub port: u16,
+    pub use_tls: bool,
     pub certchain_pem_path: String,
     pub key_pem_path: String,
 }
@@ -13,6 +14,7 @@ impl Default for Config {
         Config {
             ip_address: "127.0.0.1".to_string(),
             port: 80,
+            use_tls: false,
             certchain_pem_path: "./tls/fullchain.pem".to_string(),
             key_pem_path: "./tls/privkey.pem".to_string(),
         }
