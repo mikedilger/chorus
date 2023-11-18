3614272c4e095b8d5c3ffc95d059a4a81211f596
Mike Dilger <mike@mikedilger.com>
2023-11-19 12:31:42 +1300

    Session id generation

--- src/globals.rs
+++ src/globals.rs
@@ -2,6 +2,7 @@ use crate::config::Config;
 use crate::store::Store;
 use hyper::server::conn::Http;
 use lazy_static::lazy_static;
+use std::sync::atomic::{AtomicU64, Ordering};
 use std::sync::OnceLock;
 use tokio::sync::RwLock;
 
@@ -9,6 +10,13 @@ pub struct Globals {
     pub config: RwLock<Config>,
     pub http_server: Http,
     pub store: OnceLock<Store>,
+    pub next_session_id: AtomicU64,
+}
+
+impl Globals {
+    pub fn get_next_session_id(&self) -> u64 {
+        self.next_session_id.fetch_add(1, Ordering::Relaxed)
+    }
 }
 
 lazy_static! {
@@ -21,6 +29,7 @@ lazy_static! {
             config: RwLock::new(Config::default()),
             http_server,
             store: OnceLock::new(),
+            next_session_id: AtomicU64::new(0),
         }
     };
 }
