06d0b94d584e1a9579b7be31bc2f63e7e3984137
Mike Dilger <mike@mikedilger.com>
2023-11-19 13:10:58 +1300

    Comments about what code we still need to write

--- src/nostr.rs
+++ src/nostr.rs
@@ -10,8 +10,20 @@ pub async fn handle(_session_id: u64, _peer: SocketAddr, msg: ClientMessage) -> 
                 Ok(_) => Ok(RelayMessage::Ok(event.id, true, "stored".to_owned())),
                 Err(e) => Ok(RelayMessage::Ok(event.id, false, format!("{}", e))),
             }
+
+            // Serve to any session subscription that is listening for it
+            // TBD
         }
         ClientMessage::Req(_subid, _vec_filters) => {
+            // Store in session
+            // TBD
+
+            // Fetch until EOSE
+            // TBD
+
+            // Send EOSE
+            // TBD
+
             Ok(RelayMessage::Notice("REQ is not yet supported".to_string()))
         }
         ClientMessage::Close(_subid) => Ok(RelayMessage::Notice(
