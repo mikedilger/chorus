ee40be7625fa0fc01372e5cd9fa6c9677a662db1
Mike Dilger <mike@mikedilger.com>
2023-11-23 10:15:17 +1300

    Extend config with name, description, and pubkey

--- src/config.rs
+++ src/config.rs
@@ -4,6 +4,9 @@ use serde::{Deserialize, Serialize};
 pub struct Config {
     pub ip_address: String,
     pub port: u16,
+    pub name: Option<String>,
+    pub description: Option<String>,
+    pub public_key_hex: Option<String>,
 }
 
 impl Default for Config {
@@ -11,6 +14,9 @@ impl Default for Config {
         Config {
             ip_address: "127.0.0.1".to_string(),
             port: 80,
+            name: None,
+            description: None,
+            public_key_hex: None,
         }
     }
 }
