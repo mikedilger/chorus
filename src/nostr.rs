0deeb5e0a8719b39acb44c64ab426e30276ae601
Mike Dilger <mike@mikedilger.com>
2024-02-15 11:26:59 +1300

    If msg is large, grow the session scratch buffer

--- src/nostr.rs
+++ src/nostr.rs
@@ -6,6 +6,13 @@ use hyper_tungstenite::tungstenite::Message;
 
 impl WebSocketService {
     pub async fn handle_nostr_message(&mut self, msg: String) -> Result<(), Error> {
+        // If the msg is large, grow the session buffer
+        // (it will be freed when they disconnect)
+        if msg.len() > 4096 {
+            let newlen = 4096 * ((msg.len() / 4096) + 1);
+            self.buffer.resize(newlen, 0);
+        }
+
         log::warn!("Received unhandled text message: {}", msg);
         let reply = NostrReply::Notice("NIP-01 is not yet fully supported".to_owned());
         self.websocket.send(Message::text(reply.as_json())).await?;
