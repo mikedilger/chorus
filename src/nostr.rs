461c2d3e33470c2cd8a3e78f8737395c8e3bbb3b
Mike Dilger <mike@mikedilger.com>
2024-02-19 16:01:13 +1300

    Fix when to verify events

--- src/nostr.rs
+++ src/nostr.rs
@@ -188,7 +188,7 @@ impl WebSocketService {
 
         let event_flags = event_flags(&event, &user).await;
 
-        if GLOBALS.config.read().await.verify_events {
+        if !event_flags.author_is_an_authorized_user || GLOBALS.config.read().await.verify_events {
             // Verify the event is valid (id is hash, signature is valid)
             if let Err(e) = event.verify() {
                 return Err(ChorusError::EventIsInvalid(format!("{}", e)).into());
