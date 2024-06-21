97a8a1699944574c76ed68ff8aa50ad58415ec1f
Mike Dilger <mike@mikedilger.com>
2024-06-22 09:01:13 +1200

    update rustls 0.21->0.23;  tokio-rustls 0.24->0.26;  rustls-pemfile 1.0->2.1

--- src/tls.rs
+++ src/tls.rs
@@ -1,7 +1,7 @@
 use crate::config::Config;
 use crate::error::{ChorusError, Error};
 use crate::globals::GLOBALS;
-use rustls::{Certificate, PrivateKey};
+use rustls_pki_types::{CertificateDer, PrivateKeyDer};
 use std::fs::File;
 use std::io::BufReader;
 use std::pin::Pin;
@@ -12,18 +12,20 @@ use tokio::io::{AsyncRead, AsyncWrite, ReadBuf};
 use tokio_rustls::{rustls, TlsAcceptor};
 
 pub fn tls_acceptor(config: &Config) -> Result<TlsAcceptor, Error> {
-    let certs: Vec<Certificate> =
-        rustls_pemfile::certs(&mut BufReader::new(File::open(&config.certchain_pem_path)?))?
-            .drain(..)
-            .map(Certificate)
-            .collect();
+    let cert_file = File::open(&config.certchain_pem_path)?;
+    let mut certificates: Vec<CertificateDer<'static>> = Vec::new();
+    for maybe_cert in rustls_pemfile::certs(&mut BufReader::new(cert_file)) {
+        let cert = maybe_cert?;
+        certificates.push(cert);
+    }
 
-    let mut keys: Vec<PrivateKey> =
-        rustls_pemfile::pkcs8_private_keys(&mut BufReader::new(File::open(&config.key_pem_path)?))?
-            .drain(..)
-            .rev()
-            .map(PrivateKey)
-            .collect();
+    let key_file = File::open(&config.key_pem_path)?;
+    let mut keys: Vec<PrivateKeyDer> = Vec::new();
+    for maybe_key in rustls_pemfile::pkcs8_private_keys(&mut BufReader::new(key_file)) {
+        let key = maybe_key?;
+        keys.push(PrivateKeyDer::Pkcs8(key));
+    }
+    keys.reverse();
 
     let key = match keys.pop() {
         Some(k) => k,
@@ -31,9 +33,8 @@ pub fn tls_acceptor(config: &Config) -> Result<TlsAcceptor, Error> {
     };
 
     let tls_config = rustls::ServerConfig::builder()
-        .with_safe_defaults()
         .with_no_client_auth()
-        .with_single_cert(certs, key)?;
+        .with_single_cert(certificates, key)?;
 
     Ok(TlsAcceptor::from(Arc::new(tls_config)))
 }
